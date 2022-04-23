1574f229c507b1746966af70e79acec49943c8e8
octopi <dackogan@gmail.com>
2022-04-23 14:59:40 +0700

    changing clock time format

--- asset/js/showTime.js
+++ asset/js/showTime.js
@@ -1,10 +1,16 @@
 function display_clockTime() {
-    var e = new Date(),
-        t = e.getHours() >= 12 ? " PM" : " AM",
-        gm = e.getMonth() + 1,
-        m = gm < 10 ? "-0" + gm : "-" + gm,
-        i = "🕐 "+ e.getDate().toString() + m +  "-" + e.getFullYear();
-    (i = i + " - " + e.getHours() + ":" + e.getMinutes() + ":" + e.getSeconds() + t), (document.getElementById("clockTime").innerHTML = i), display_c5();
+    var date = new Date(),
+        timePeriod = date.getHours() >= 12 ? " PM" : " AM",
+        getMonth = date.getMonth() + 1,
+        getHours = date.getHours(),
+        getMins = date.getMinutes(),
+        getSecs = date.getSeconds();
+        format_getMonth = getMonth < 10 ? "-0" + getMonth : "-" + getMonth,
+        format_getHours = getHours > 12 ? getHours-12 : getHours;
+        format_getMins = getMins < 10 ? ":0" + getMins : ":" + getMins,
+        format_getSecs = getSecs < 10 ? ":0" + getSecs : ":" + getSecs,
+        i = "🕐 "+ date.getDate().toString() + format_getMonth +  "-" + date.getFullYear();
+    (i = i + " • " + format_getHours + format_getMins + format_getSecs + timePeriod), (document.getElementById("clockTime").innerHTML = i), display_c5();
 }
 function display_c5() {
     mytime = setTimeout("display_clockTime()", 1e3);
