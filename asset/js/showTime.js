9442e59d05cea1df85754ae2ade27752acb3262f
Xaypanya Phongsa <dackogan@gmail.com>
2022-05-03 10:12:59 +0700

    adding formatDay

--- asset/js/showTime.js
+++ asset/js/showTime.js
@@ -2,14 +2,16 @@ function display_clockTime() {
     var date = new Date(),
         timePeriod = date.getHours() >= 12 ? " PM" : " AM",
         getMonth = date.getMonth() + 1,
+        getDay = date.getDay() + 1; 
         getHours = date.getHours(),
         getMins = date.getMinutes(),
         getSecs = date.getSeconds();
         format_getMonth = getMonth < 10 ? "-0" + getMonth : "-" + getMonth,
+        format_getDay = getDay < 10 ? "0" + getDay : getDay,
         format_getHours = getHours > 12 ? getHours-12 : getHours;
         format_getMins = getMins < 10 ? ":0" + getMins : ":" + getMins,
         format_getSecs = getSecs < 10 ? ":0" + getSecs : ":" + getSecs,
-        i = "🕐 "+ date.getDate().toString() + format_getMonth +  "-" + date.getFullYear();
+        i = "🕐 "+ format_getDay + format_getMonth +  "-" + date.getFullYear();
     (i = i + " ◃▹ " + format_getHours + format_getMins + format_getSecs + timePeriod), (document.getElementById("clockTime").innerHTML = i), display_c5();
 }
 function display_c5() {
